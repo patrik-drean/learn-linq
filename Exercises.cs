4a03fdaf798f2ca4fe2c124d1d86b7dd77b1307c
Patrik Drean <patrikdrean@gmail.com>
2021-09-08 11:52:55 -0600

    Initial commit

---
+++ Exercises.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Bank
+{
+    public string Symbol { get; set; }
+    public string Name { get; set; }
+}
+
+public class Customer
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Bank { get; set; }
+}
+
+public class GroupedMillionaires {
+    public string Bank { get; set; }
+    public IEnumerable<string> Millionaires { get; set; }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find the words in the collection that start with the letter 'L'
+        List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            // Which of the following numbers are multiples of 4 or 6
+        List<int> mixedNumbers = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+
+        // Order the names alphabetically
+        List<string> names = new List<string>()
+        {
+            "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+            "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+            "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+            "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+            "Francisco", "Tre"
+        };
+
+        // Output how many numbers are in this list
+        List<int> numbers = new List<int>()
+        {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+        };
+
+        // How much money have we made?
+        List<double> purchases = new List<double>()
+        {
+            2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+        };
+
+        // What is our most expensive product?
+        List<double> prices = new List<double>()
+        {
+            879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+        };
+
+        /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+        */
+
+        List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+        var groupedByBank = customers.Where(c => c.Balance >= 1000000).GroupBy(
+            p => p.Bank,  // Group banks
+            p => p.Name,  // by millionaire names
+            (bank, millionaires) => new GroupedMillionaires()
+            {
+                Bank = bank,
+                Millionaires = millionaires
+            }
+        ).ToList();
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,108 @@
+# What is LINQ?
+LINQ is a standard query syntax in C# that abstracts the logic of retrieving data.
+
+![](2021-09-08-08-22-22.png)
+
+The datasource could be a variety of things, here's just a few: 
+* A list
+* An object
+* JSON
+* A database
+
+# Query Syntax vs Method Syntax
+
+## Query Syntax:
+```C#
+var names = new List<string>() { 
+    "Shawn",
+    "Gus"
+};
+
+var result = from name in names
+            where name.Contains("Shawn") 
+            select name;
+```
+
+## Method Syntax:
+```C#
+var names = new List<string>() { 
+    "Shawn",
+    "Gus"
+};
+
+var result = names.Where(name => name.Contains("Shawn"));
+```
+
+
+# Why use LINQ?
+* Standard, familiar language
+* More readable
+* Type safety
+  
+  
+# Which is easer to understand?
+## Option 1: For Loop
+  ```C#
+  var students = { 
+        new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+        new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+        new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+        new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+        new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+        new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+        new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+    };
+
+    var newStudents = new Student[10];
+
+    int i = 0;
+
+    foreach (Student std in newStudents)
+    {
+        if (std.Age > 12 && std.Age < 20)
+        {
+            newStudents[i] = std;
+            i++;
+        }
+    }
+```
+
+## Option 2: LINQ
+```C#
+var students = { 
+        new Student() { StudentID = 1, StudentName = "John", age = 18 } ,
+        new Student() { StudentID = 2, StudentName = "Steve",  age = 21 } ,
+        new Student() { StudentID = 3, StudentName = "Bill",  age = 25 } ,
+        new Student() { StudentID = 4, StudentName = "Ram" , age = 20 } ,
+        new Student() { StudentID = 5, StudentName = "Ron" , age = 31 } ,
+        new Student() { StudentID = 6, StudentName = "Chris",  age = 17 } ,
+        new Student() { StudentID = 7, StudentName = "Rob",age = 19  } ,
+    };
+
+    var teenagerStudents = students.Where(s => s.age > 12 && s.age < 20);
+```
+
+# Common LINQ Operators
+```C#
+var students = { 
+        new Student() { StudentID = 1, StudentName = "John", age = 18 } ,
+        new Student() { StudentID = 2, StudentName = "Steve",  age = 21 } ,
+        new Student() { StudentID = 3, StudentName = "Bill",  age = 25 } ,
+        new Student() { StudentID = 4, StudentName = "Ram" , age = 20 } ,
+        new Student() { StudentID = 5, StudentName = "Ron" , age = 31 } ,
+        new Student() { StudentID = 6, StudentName = "Chris",  age = 17 } ,
+        new Student() { StudentID = 7, StudentName = "Rob",age = 19  } ,
+    };
+
+var teenagerStudents = students.Where(s => s.age > 12 && s.age < 20);
+var studentsOrdered = students.OrderByDescending(s => s.age);
+var averageAge = students.Average(s => s.age);
+var studentCount = students.Count();
+var uniqueStudents = students.Discount();
+
+```
+
+# Exercises
+
+# Feedback
+https://forms.gle/Vbx1hiszfV9sKQkZ8
